1b7d8af9407cfe58a74cebeb81b2f2aad95070b1
Conor Gilsenan <conorgilsenan@gmail.com>
2013-04-15 13:21:02 -0400

    adds logic to register/unregister sms receiver on button click

--- src/com/example/foobar/MainActivity.java
+++ src/com/example/foobar/MainActivity.java
@@ -13,9 +13,12 @@ public class MainActivity extends Activity {
 	public static final String EXTRA_MESSAGE = THIS_NAME + ".EXTRA_MESSAGE";
 
 	private boolean isSmsReceiverEnabled = false;
-	
-	// TODO probably don't create static final instance - need to pass emails to this somehow
-	private static final SmsReceiver SMS_RECEIVER = new SmsReceiver(); 
+
+	// TODO probably don't create static final instance - need to pass emails to
+	// this somehow
+	private static final SmsReceiver SMS_RECEIVER = new SmsReceiver();
+	private static final IntentFilter SMS_RECEIVED_INTENT_FILTER = new IntentFilter(
+			"android.provider.Telephony.SMS_RECEIVED");
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +26,9 @@ public class MainActivity extends Activity {
 		System.out.println("MainActivity onCreate");
 		setContentView(R.layout.activity_main);
 
-		//TODO read the enabled/disabled setting from file or the savedInstanceState
-		
+		// TODO read the enabled/disabled setting from file or the
+		// savedInstanceState
+
 	}
 
 	@Override
@@ -37,7 +41,7 @@ public class MainActivity extends Activity {
 	public void btn_registerSmsReceiver_onClick(View view) {
 		System.out.println("btn_registerSmsReceiver_onClick");
 		Button btn = (Button) findViewById(R.id.btn_registerSmsReceiver);
-		if(isSmsReceiverEnabled) {
+		if (isSmsReceiverEnabled) {
 			unregisterSmsReceiver();
 			btn.setText(R.string.btn_registerSmsReceiver_unregisteredText);
 		} else {
@@ -49,20 +53,24 @@ public class MainActivity extends Activity {
 
 	private void registerSmsReceiver() {
 		System.out.println("registerSmsReceiver()");
-		getApplicationContext().registerReceiver(SMS_RECEIVER, new IntentFilter(action));
+		getApplicationContext().registerReceiver(SMS_RECEIVER,
+				SMS_RECEIVED_INTENT_FILTER);
+		// TODO would be cool to put an icon in the notification bar to show
+		// that forwarding is on
 	}
 
 	private void unregisterSmsReceiver() {
 		System.out.println("unregisterSmsReceiver()");
+		getApplicationContext().unregisterReceiver(SMS_RECEIVER);
 	}
 
 	public void btn_addEmail_onClick(View view) {
 		System.out.println("btn_addEmail_onClick()");
 		EditText addEmailBox = (EditText) findViewById(R.id.btn_addEmail);
 		addEmailBox.getText();
-		
+
 	}
-	
+
 	// Functions just for random curiosity/debug/exploration
 	@Override
 	public void onPause() {
@@ -99,7 +107,7 @@ public class MainActivity extends Activity {
 		super.onDestroy();
 		System.out.println("MainActivity destroy");
 	}
-	
+
 	// public void btn_sendMessage_onClick(View view) {
 	// System.out.println("clicked the button!");
 	// Intent intent = new Intent(this, PublishMessageActivity.class);
